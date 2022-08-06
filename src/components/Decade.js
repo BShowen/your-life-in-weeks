4e00176686194d594994a540651f36397d5e6472
Bradley Showen <bshowen@live.seminolestate.edu>
2022-08-06 19:19:27 -0400

    Create the Decade component

---
+++ src/components/Decade.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Stack } from "react-bootstrap";
+import Year from "./Year";
+
+export default class Decade extends Component {
+  decade() {
+    const years = new Array(10);
+    for (let i = 0; i < years.length; i++) {
+      years[i] = <Year key={i} {...this.props} />;
+    }
+    return years;
+  }
+
+  render() {
+    return <Stack gap={2}>{this.decade()}</Stack>;
+  }
+}
