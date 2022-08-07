7eb50eb7ceb8ad329d4cf482bdab0de0fc53ed4d
Bradley Showen <bshowen@live.seminolestate.edu>
2022-08-06 22:06:22 -0400

    Style the navigation and setup click handlers for button active status

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,28 +9,56 @@ export default class NavBar extends Component {
 
     this.state = {
       navButtons: [
-        { title: "Text 1", color: "red" },
-        { title: "Text 2", color: "green" },
+        { title: "Text 1", color: "#FBAE14", active: false, id: 0 },
+        { title: "Text 2", color: "#019BFD", active: false, id: 1 },
+        { title: "Text 2", color: "#00F0AC", active: false, id: 2 },
+        { title: "Text 2", color: "#E10075", active: false, id: 3 },
       ],
     };
 
     this.createNewCategory = this.createNewCategory.bind(this);
+    this.setActiveNavButton = this.setActiveNavButton.bind(this);
   }
 
   navButtons() {
     const navButtons = this.state.navButtons.map((button, i) => {
-      return <NavButton key={i} {...button} />;
+      return (
+        <NavButton key={i} {...button} clickHandler={this.setActiveNavButton} />
+      );
     });
-    return <Container fluid>{navButtons}</Container>;
+    return (
+      <Container
+        fluid
+        className="d-flex flex-column justify-content-start"
+        style={{ gap: "15px" }}
+      >
+        {navButtons}
+      </Container>
+    );
   }
 
   createNewCategory() {
     console.log("NavAddButton clicked");
   }
 
+  setActiveNavButton(id) {
+    this.setState({
+      navButtons: this.state.navButtons.map((navButton) => {
+        if (navButton.id === id) {
+          return { ...navButton, active: true };
+        } else {
+          return { ...navButton, active: false };
+        }
+      }),
+    });
+  }
+
   render() {
     return (
-      <Nav className="flex-column w-100">
+      <Nav
+        className="w-100 h-100 p-3 flex-column justify-content-between"
+        style={{ backgroundColor: "#000000" }}
+      >
         {this.navButtons()}
         <NavAddButton clickHandler={this.createNewCategory} />
       </Nav>
--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -2,13 +2,34 @@ import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 
 export default class NavButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.props.clickHandler(this.props.id);
+  }
+
   render() {
     const { title, color } = this.props;
+    const borderColor = this.props.active ? color : "#8610A8";
     return (
       <Container
         fluid
         as="div"
-        className="d-flex flex-row flex-nowrap flex-content-between"
+        className="d-flex flex-row flex-nowrap flex-content-between align-items-center"
+        style={{
+          backgroundColor: "#8610A8",
+          border: `2px solid ${borderColor}`,
+          height: "50px",
+          gap: "10px",
+          color: "#FFFFFF",
+          borderRadius: "12px",
+        }}
+        onClick={this.handleClick}
       >
         <div
           style={{
@@ -19,7 +40,7 @@ export default class NavButton extends Component {
           }}
         ></div>
         <div>
-          <p>{title}</p>
+          <p className="p-0 m-0">{title}</p>
         </div>
       </Container>
     );
