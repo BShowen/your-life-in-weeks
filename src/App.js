90bcaacfe3c3db030e49465092adac96bf2bfafd
Bradley Showen <bshowen@live.seminolestate.edu>
2022-08-06 21:26:04 -0400

    Convert App.js from function into ES6 React class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return <p>Hello World!</p>;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Container fluid className="h-100">
+        <Row className="h-100">
+          <Col lg={3} className="ps-0 pe-0 h-100">
+            <NavBar />
+          </Col>
+          <Col lg={9} className="ps-0 pe-0 h-100">
+            Column 2
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
-
-export default App;
