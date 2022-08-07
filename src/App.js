b63695d06e805b76cc76b8ffb5591aa412f5b1e3
Bradley Showen <bshowen@live.seminolestate.edu>
2022-08-06 23:07:18 -0400

    Style app so calendar scrolls and navigation sticks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Calendar from "./components/Calendar";
 
 export default class App extends Component {
   constructor(props) {
@@ -14,11 +15,15 @@ export default class App extends Component {
     return (
       <Container fluid className="h-100">
         <Row className="h-100">
-          <Col lg={3} className="ps-0 pe-0 h-100">
+          <Col lg={2} className="ps-0 pe-0 h-100">
             <NavBar />
           </Col>
-          <Col lg={9} className="ps-0 pe-0 h-100">
-            Column 2
+          <Col
+            lg={10}
+            className="ps-0 pe-0 h-100"
+            style={{ overflow: "scroll" }}
+          >
+            <Calendar />
           </Col>
         </Row>
       </Container>
