3460d979962812a7cf9b5966aa966d3d607ccdc3
Bradley Showen <bshowen@live.seminolestate.edu>
2022-08-06 23:07:50 -0400

    Create the Calendar component

---
+++ src/components/Calendar.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Decade from "./Decade";
+
+export default class Calendar extends Component {
+  decades() {
+    const decades = new Array(10);
+    for (let i = 0; i < decades.length; i++) {
+      // {i * 10} to {(i + 1) * 10 - 1}
+      decades[i] = <Decade key={i} />;
+    }
+    return decades;
+  }
+
+  weekNumbers() {
+    const weekNumbers = new Array(52);
+    for (let i = 0; i < weekNumbers.length; i++) {
+      weekNumbers[i] = (
+        <p
+          key={i}
+          className="text-light text-center p-0 m-0"
+          style={{ width: "25px" }}
+        >
+          {(i + 1).toString()}
+        </p>
+      );
+    }
+    return (
+      <div className="d-flex flex-row justify-content-between flex-nowrap mb-3">
+        {weekNumbers}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div
+        style={{ backgroundColor: "#000000" }}
+        gap={1}
+        className="ps-2 pe-4 d-flex flex-column align-items-center"
+      >
+        <div className="d-flex justify-content-center align-items-center p-5">
+          <h1 className="p-0 m-0 text-light">Your life in weeks</h1>
+        </div>
+        <div className="w-75">
+          {this.weekNumbers()}
+          {this.decades()}
+        </div>
+      </div>
+    );
+  }
+}
