f26e4768ddc148aded9420c0901f2410c12ef046
Bradley Showen <bshowen@live.seminolestate.edu>
2022-08-13 16:06:18 -0400

    Style the MiniNav button

--- src/components/TippyButton.js
+++ src/components/TippyButton.js
@@ -4,10 +4,17 @@ import { FaChevronCircleUp } from "react-icons/fa";
 export default function TippyButton(props) {
   const { toggleActive, active } = props;
   return (
-    <Button variant="primary" onClick={toggleActive}>
-      <div className={"d-flex justify-content-center align-items-center"}>
-        <FaChevronCircleUp className={`chevron ${active ? "rotate" : ""}`} />
-      </div>
+    <Button
+      variant="info"
+      size="lg"
+      onClick={toggleActive}
+      style={{ height: "3rem", width: "3rem" }}
+      className={"d-flex justify-content-center align-items-center p-1"}
+    >
+      <FaChevronCircleUp
+        className={`chevron ${active ? "rotate" : ""}`}
+        style={{ minWidth: "100%", minHeight: "100%" }}
+      />
     </Button>
   );
 }
